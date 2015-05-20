9aaf8e654114125298f58736923ab7d0bc8c18a9
alex-godro <alex_godro@hotmail.com>
2015-05-19 22:59:43 -0400

    Method parameter

--- ZETP/ZETP/Interface/FormMethodParameters.Designer.cs
+++ ZETP/ZETP/Interface/FormMethodParameters.Designer.cs
@@ -28,33 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.TbxParameter5 = new System.Windows.Forms.TextBox();
+            this.TbxParameter4 = new System.Windows.Forms.TextBox();
+            this.TbxParameter3 = new System.Windows.Forms.TextBox();
+            this.TbxParameter2 = new System.Windows.Forms.TextBox();
             this.LblParameter1 = new System.Windows.Forms.Label();
             this.LblParameter5 = new System.Windows.Forms.Label();
             this.LblParameter4 = new System.Windows.Forms.Label();
             this.LblParameter3 = new System.Windows.Forms.Label();
             this.LblParameter2 = new System.Windows.Forms.Label();
+            this.TbxParameter1 = new System.Windows.Forms.TextBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.BtnCancel = new System.Windows.Forms.Button();
             this.BtnOk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
+            // errorProvider
+            // 
+            this.errorProvider.BlinkRate = 0;
+            this.errorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider.ContainerControl = this;
+            // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.TbxParameter5, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.TbxParameter4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.TbxParameter3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TbxParameter2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.LblParameter1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.LblParameter5, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.LblParameter4, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.LblParameter3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.LblParameter2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TbxParameter1, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 10);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
@@ -63,6 +84,54 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(368, 133);
             this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // TbxParameter5
+            // 
+            this.TbxParameter5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TbxParameter5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxParameter5.Location = new System.Drawing.Point(76, 99);
+            this.TbxParameter5.Margin = new System.Windows.Forms.Padding(3, 3, 20, 3);
+            this.TbxParameter5.Name = "TbxParameter5";
+            this.TbxParameter5.Size = new System.Drawing.Size(272, 20);
+            this.TbxParameter5.TabIndex = 9;
+            this.TbxParameter5.Visible = false;
+            this.TbxParameter5.Validating += new System.ComponentModel.CancelEventHandler(this.TbxParameter_Validating);
+            // 
+            // TbxParameter4
+            // 
+            this.TbxParameter4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TbxParameter4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxParameter4.Location = new System.Drawing.Point(76, 75);
+            this.TbxParameter4.Margin = new System.Windows.Forms.Padding(3, 3, 20, 3);
+            this.TbxParameter4.Name = "TbxParameter4";
+            this.TbxParameter4.Size = new System.Drawing.Size(272, 20);
+            this.TbxParameter4.TabIndex = 8;
+            this.TbxParameter4.Visible = false;
+            this.TbxParameter4.Validating += new System.ComponentModel.CancelEventHandler(this.TbxParameter_Validating);
+            // 
+            // TbxParameter3
+            // 
+            this.TbxParameter3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TbxParameter3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxParameter3.Location = new System.Drawing.Point(76, 51);
+            this.TbxParameter3.Margin = new System.Windows.Forms.Padding(3, 3, 20, 3);
+            this.TbxParameter3.Name = "TbxParameter3";
+            this.TbxParameter3.Size = new System.Drawing.Size(272, 20);
+            this.TbxParameter3.TabIndex = 7;
+            this.TbxParameter3.Visible = false;
+            this.TbxParameter3.Validating += new System.ComponentModel.CancelEventHandler(this.TbxParameter_Validating);
+            // 
+            // TbxParameter2
+            // 
+            this.TbxParameter2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TbxParameter2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxParameter2.Location = new System.Drawing.Point(76, 27);
+            this.TbxParameter2.Margin = new System.Windows.Forms.Padding(3, 3, 20, 3);
+            this.TbxParameter2.Name = "TbxParameter2";
+            this.TbxParameter2.Size = new System.Drawing.Size(272, 20);
+            this.TbxParameter2.TabIndex = 6;
+            this.TbxParameter2.Visible = false;
+            this.TbxParameter2.Validating += new System.ComponentModel.CancelEventHandler(this.TbxParameter_Validating);
             // 
             // LblParameter1
             // 
@@ -75,18 +144,20 @@
             this.LblParameter1.TabIndex = 0;
             this.LblParameter1.Text = "Parameter 1:";
             this.LblParameter1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LblParameter1.Visible = false;
             // 
             // LblParameter5
             // 
             this.LblParameter5.AutoSize = true;
             this.LblParameter5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LblParameter5.Location = new System.Drawing.Point(3, 99);
-            this.LblParameter5.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.LblParameter5.Location = new System.Drawing.Point(3, 102);
+            this.LblParameter5.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
             this.LblParameter5.Name = "LblParameter5";
-            this.LblParameter5.Size = new System.Drawing.Size(67, 21);
+            this.LblParameter5.Size = new System.Drawing.Size(67, 31);
             this.LblParameter5.TabIndex = 4;
             this.LblParameter5.Text = "Parameter 5:";
-            this.LblParameter5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LblParameter5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.LblParameter5.Visible = false;
             // 
             // LblParameter4
             // 
@@ -99,6 +170,7 @@
             this.LblParameter4.TabIndex = 3;
             this.LblParameter4.Text = "Parameter 4:";
             this.LblParameter4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LblParameter4.Visible = false;
             // 
             // LblParameter3
             // 
@@ -111,6 +183,7 @@
             this.LblParameter3.TabIndex = 2;
             this.LblParameter3.Text = "Parameter 3:";
             this.LblParameter3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LblParameter3.Visible = false;
             // 
             // LblParameter2
             // 
@@ -123,6 +196,19 @@
             this.LblParameter2.TabIndex = 1;
             this.LblParameter2.Text = "Parameter 2:";
             this.LblParameter2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LblParameter2.Visible = false;
+            // 
+            // TbxParameter1
+            // 
+            this.TbxParameter1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TbxParameter1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxParameter1.Location = new System.Drawing.Point(76, 3);
+            this.TbxParameter1.Margin = new System.Windows.Forms.Padding(3, 3, 20, 3);
+            this.TbxParameter1.Name = "TbxParameter1";
+            this.TbxParameter1.Size = new System.Drawing.Size(272, 20);
+            this.TbxParameter1.TabIndex = 5;
+            this.TbxParameter1.Visible = false;
+            this.TbxParameter1.Validating += new System.ComponentModel.CancelEventHandler(this.TbxParameter_Validating);
             // 
             // flowLayoutPanel1
             // 
@@ -153,13 +239,12 @@
             this.BtnOk.TabIndex = 1;
             this.BtnOk.Text = "Ok";
             this.BtnOk.UseVisualStyleBackColor = true;
+            this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click);
             // 
             // FormMethodParameters
             // 
-            this.AcceptButton = this.BtnOk;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.BtnCancel;
             this.ClientSize = new System.Drawing.Size(388, 184);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -172,23 +257,32 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Methods Parameters";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.ErrorProvider errorProvider;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox TbxParameter5;
+        private System.Windows.Forms.TextBox TbxParameter4;
+        private System.Windows.Forms.TextBox TbxParameter3;
+        private System.Windows.Forms.TextBox TbxParameter2;
         private System.Windows.Forms.Label LblParameter1;
         private System.Windows.Forms.Label LblParameter5;
         private System.Windows.Forms.Label LblParameter4;
         private System.Windows.Forms.Label LblParameter3;
         private System.Windows.Forms.Label LblParameter2;
+        private System.Windows.Forms.TextBox TbxParameter1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button BtnCancel;
         private System.Windows.Forms.Button BtnOk;
+
     }
 }
